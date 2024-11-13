f24372e6b04a1bebe205c89a1322820c733506bb
Herman Cruz Jr. <herman_cruz@outlook.ph>
2024-11-13 21:24:21 +0800

    Add final code for experimenting controls

--- ExperimentWithControls/MainPage.xaml.cs
+++ ExperimentWithControls/MainPage.xaml.cs
@@ -7,6 +7,14 @@
         public MainPage()
         {
             InitializeComponent();
+            BirdPicker.ItemsSource = new String[]
+            {
+                "Duck",
+                "Pigeon",
+                "Penguin",
+                "Ostrich",
+                "Owl"
+            };
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -34,6 +42,15 @@
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             SliderValue.Text = e.NewValue.ToString();
+        }
+
+        private void AddBird_Clicked(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(Birds.Text))
+            {
+                Birds.Text += Environment.NewLine;
+            }
+            Birds.Text += BirdPicker.SelectedItem;
         }
     }
 
