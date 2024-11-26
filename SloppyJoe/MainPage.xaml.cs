b6e385d6d9d438fac9b6d3c082614dfc7ed7ff07
Amnher <amnher@gmail.com>
2024-11-26 18:24:40 +0800

    Initial SloppyJoe MAUI commits

--- SloppyJoe/MainPage.xaml.cs
+++ SloppyJoe/MainPage.xaml.cs
@@ -2,24 +2,38 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
             InitializeComponent();
+            MakeTheMenu();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void MakeTheMenu()
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            MenuItem[] menuItems = new MenuItem[6];
+            for (int i = 0; i < 6; i++)
+            {
+                menuItems[i] = new MenuItem();
+                menuItems[i].Generate();
+            }
+            price1.Text = menuItems[0].Price;
+            item1.Text = menuItems[0].Description;
+            price2.Text = menuItems[1].Price;
+            item2.Text = menuItems[1].Description;
+            price3.Text = menuItems[2].Price;
+            item3.Text = menuItems[2].Description;
+            price4.Text = menuItems[3].Price;
+            item4.Text = menuItems[3].Description;
+            price5.Text = menuItems[4].Price;
+            item5.Text = menuItems[4].Description;
+            price6.Text = menuItems[5].Price;
+            item6.Text = menuItems[5].Description;
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamole.Text = "Add guacamole for " + guacamoleMenuItem.Price;
         }
+
     }
 
 }
---
+++ SloppyJoe/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoe
+{
+    internal class MenuItem
+    {
+        public string[] Proteins = [
+                    "Roast beef", "Salami", "Turkey",
+                    "Ham", "Pastrami", "Tofu"
+        ];
+        public string[] Condiments = [
+                    "yellow mustard", "brown mustard",
+                    "honey mustard", "mayo", "relish", "French dressing"
+        ];
+        public string[] Breads = ["rye", "white", "wheat", "pumpernickel", "a roll"];
+        public string Description = "";
+        public string Price = "";
+        public void Generate()
+        {
+            // You'll fill in this method
+        }
+    }
+}
